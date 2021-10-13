ed810f29205f3883ec2b755d972dfe5872e2a76a
xrenna <renna961596@163.com>
2021-10-13 22:35:54 +0800

    writes basic layout for stories

---
+++ src/components/Stories.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Stories = () => {
+    return (
+        <section className='stories'>
+            <div className='stories__box'>
+                <p className='stories__text'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae dolor et felis ullamcorper posuere vitae ut ante. Morbi eu auctor erat. Morbi venenatis commodo enim, vel auctor ex ullamcorper porttitor. Curabitur sollicitudin vel lacus a feugiat. Quisque convallis sodales nunc in malesuada. Maecenas suscipit odio in ante pellentesque, et lobortis tellus ultrices. Etiam odio mi, fermentum quis sem ut, lobortis pulvinar dui. Suspendisse vitae lorem convallis, eleifend diam id, ullamcorper arcu.
+                </p>
+                <img className='stories__img'></img>
+            </div>
+        </section>
+    )
+}
+
+export default Stories
