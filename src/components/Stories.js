b07424b8b74cb0fbb3eeac4cbe18dbfeef1d9744
xrenna <renna961596@163.com>
2021-10-15 22:54:37 +0800

    adds images for user stories, imports man1 into stories component and adds it as the src

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Man1 from '../imgs/man1.jpg'
 
 const Stories = () => {
     return (
@@ -7,7 +8,7 @@ const Stories = () => {
                 <p className='stories__text'>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae dolor et felis ullamcorper posuere vitae ut ante. Morbi eu auctor erat. Morbi venenatis commodo enim, vel auctor ex ullamcorper porttitor. Curabitur sollicitudin vel lacus a feugiat. Quisque convallis sodales nunc in malesuada. Maecenas suscipit odio in ante pellentesque, et lobortis tellus ultrices. Etiam odio mi, fermentum quis sem ut, lobortis pulvinar dui. Suspendisse vitae lorem convallis, eleifend diam id, ullamcorper arcu.
                 </p>
-                <img className='stories__img'></img>
+                <img className='stories__img' src={Man1} alt='User 1' />
             </div>
         </section>
     )
