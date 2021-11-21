08c52a1464f34a0832a01eb664260d1b7fa8ee2b
xrenna <renna961596@163.com>
2021-11-21 22:01:01 +0800

    updates footer to include logos for github, linkedin, and mail

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Logo from '../imgs/Habitual.png'
+import sprite from '../imgs/sprite.svg'
 
 const Footer = () => {
     return (
@@ -8,7 +9,15 @@ const Footer = () => {
             <img src={ Logo } className='footer__logo' />
         </div>
         <div className='footer__links'>
-
+            <svg className="icon icon--edit">
+                <use href={sprite + '#icon-linkedin'} />
+            </svg>
+            <svg className="icon icon--edit">
+                <use href={sprite + '#icon-github'} />
+            </svg>
+            <svg className="icon icon--edit">
+                <use href={sprite + '#icon-mail'} />
+            </svg>
         </div>
         <div>
             <p>&copy; Jordan T. Romero 2021</p>
